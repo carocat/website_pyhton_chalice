1b9b2bbd2ec9230e94b4405701ab94052c5a17b0
Carolina Abs Da Cruz <>
2021-04-30 14:23:16 +0200

    add more pages

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 import os
-import sys
 
-import boto3
 import json, plotly
 from jinja2 import Environment
 from jinja2 import FileSystemLoader
 from chalice import Chalice, Response, NotFoundError, ChaliceViewError, BadRequestError
-from wrangling_scripts.wrangle_data import return_figures
+from wrangling_scripts.wrangle_data import return_figures, return_figures_first, return_figures_second
 
 from logging import basicConfig
 log = basicConfig(level='DEBUG')
@@ -42,31 +40,39 @@ def index():
                     headers={'Content-Type': 'text/html'})
 
 
-@app.route('/inference', methods=['POST'], content_types=['application/x-www-form-urlencoded'])
+@app.route('/first', methods=['GET'])
 def inference():
 
-    if sys.version_info[0] == 3:
-        from urllib.parse import parse_qs
-    # Python 3 imports.
+    figures = return_figures_first()
 
-    else:
-        from urlparse import parse_qs
-    # Python 2 imports.
+    # plot ids for the html id tag
+    ids = ['figure-{}'.format(i) for i, _ in enumerate(figures)]
 
-    parsed = parse_qs(app.current_request.raw_body.decode())
-    content = parsed["content"][0]
+    # Convert the plotly figures to JSON for javascript in html template
+    figuresJSON = json.dumps(figures, cls=plotly.utils.PlotlyJSONEncoder)
 
-    if "SAGEMAKER_ENDPOINT" not in os.environ or "REPLACE_WITH" in os.environ["SAGEMAKER_ENDPOINT"]:
-        raise ChaliceViewError("No SAGEMAKER_ENDPOINT configured")
 
-    client = boto3.client('sagemaker-runtime')
-    response = client.invoke_endpoint(
-        EndpointName=os.environ["SAGEMAKER_ENDPOINT"],
-        Body=content,
-        ContentType='text/plain'
-    )
-
-    inference = response['Body'].read().decode()
-    return Response(_render_template(inference = inference),
+    return Response(_render_template(ids=ids,
+                                     figuresJSON=figuresJSON),
                     status_code=200,
                     headers={'Content-Type': 'text/html'})
+
+@app.route('/second', methods=['GET'])
+def inference():
+
+    figures = return_figures_second()
+
+    # plot ids for the html id tag
+    ids = ['figure-{}'.format(i) for i, _ in enumerate(figures)]
+
+    # Convert the plotly figures to JSON for javascript in html template
+    figuresJSON = json.dumps(figures, cls=plotly.utils.PlotlyJSONEncoder)
+
+
+    return Response(_render_template(ids=ids,
+                                     figuresJSON=figuresJSON),
+                    status_code=200,
+                    headers={'Content-Type': 'text/html'})
+
+
+
